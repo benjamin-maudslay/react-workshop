2a4bb275cba904c1b03eb8d2fad0289d3fb65123
Benjamin Maudslay <benjamin.maudslay@sykescottages.co.uk>
2024-02-08 19:12:32 +0000

    Intro to components

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,22 @@
 const App = () => {
   return (
     <div>
-      <h1>Hello World</h1>
+      <Title />
+      <Content />
     </div>
+  )
+}
+
+const Title = () => {
+  return (
+    <h1>Einstein - Sykes Property Management CMS</h1>
+  )
+}
+
+const Content = () => {
+  return (
+    <strong>WIP</strong>
+    <p>Welcome to einstein. <br /> Here you will find every tool you need to manage properties uploaded to the Sykes site.</p>
   )
 }
 
